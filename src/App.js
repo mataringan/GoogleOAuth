d1813685ec2ce9396bbd7fb8bffd3932b1004dbf
taofikarianto <taofikarianto@gmail.com>
2023-05-08 16:26:01 +0700

    add GoogleOAuth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { gapi } from "gapi-script";
+import Login from "./components/login";
+import Logout from "./components/logout";
+
+const clientId = process.env.REACT_APP_CLIENT_ID;
 
 function App() {
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        scope: "",
+      });
+    }
+    gapi.load("client:auth2", start);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login />
+      <Logout />
     </div>
   );
 }
